df91e673da86872ad69085d8bc17ca1d6e037f2f
Sander Lukas <31885464+sanderlukas@users.noreply.github.com>
2018-06-14 19:45:13 +0300

    Updated

--- popup.js
+++ popup.js
@@ -1,22 +1,27 @@
+'use strict'
+
 const sizeUpBtn = document.getElementById('sizeUp')
 const sizeDownBtn = document.getElementById('sizeDown')
-
-if (document.body.style.fontSize === '') {
-    document.body.style.fontSize = '12px'
-}
+let size = 12
 
 sizeUpBtn.onclick = function () {
     console.log("suurem")
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
-            {code: 'document.body.style.fontSize = "' + parseInt(document.body.style.fontSize.substring(0, 2)) + 5 + 'px";'})
+            {code: 'document.body.style.fontSize = "' + size + 'px"'})
       })
+    size = size + 2
 }
 
 sizeDownBtn.onclick = function () {
     console.log("väiksem")
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
-            {code: 'document.body.style.fontSize = "' + parseInt(document.body.style.fontSize.substring(0, 2)) - 5 + 'px";'})
+            {code: 'document.body.style.fontSize = "' + size + 'px"'})
       })
+    if (size <= 8) {
+      size = 8
+    } else {
+      size = size - 2
+    }
 }
